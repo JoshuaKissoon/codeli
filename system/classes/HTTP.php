31e2ab399fefecbfaff071848c1149d52b465d03
Joshua Kissoon <JoshuaKissoon@gmail.com>
2015-01-03 16:27:20 +0800

    Added an HTTP Class

---
+++ system/classes/HTTP.php
@@ -0,0 +1,18 @@
+<?php
+
+    /**
+     * Class with various HTTP functionality
+     *
+     * @author Joshua Kissoon
+     * @since 20150103
+     */
+    class HTTP
+    {
+
+        const METHOD_GET = "GET";
+        const METHOD_POST = "POST";
+        const METHOD_REQUEST = "REQUEST";
+        const METHOD_DELETE = "DELETE";
+
+    }
+    
