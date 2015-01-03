686cbbdad1f523ec28143a503ff2e7a38f295a98
Joshua Kissoon <JoshuaKissoon@gmail.com>
2015-01-03 16:13:22 +0800

    Setted up the route class

--- system/classes/Route.php
+++ system/classes/Route.php
@@ -8,5 +8,39 @@
      */
     class Route
     {
+
+        private $url;
+        private $callback;
+        private $permission;
+        private $httpMethod;
+
+        public function __construct($url, $callback, $permission = "", $httpMethod = "GET")
+        {
+            $this->url = $url;
+            $this->callback = $callback;
+            $this->permission = $permission;
+            $this->httpMethod = $httpMethod;
+        }
+
+        public function getURL()
+        {
+            return $this->url;
+        }
+
+        public function getCallback()
+        {
+            return $this->callback;
+        }
+
+        public function getPermission()
+        {
+            return $this->permission;
+        }
+
+        public function getHTTPMethod()
+        {
+            return $this->httpMethod;
+        }
+
     }
     
