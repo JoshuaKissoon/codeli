96f417bddbeaaac51e3be6400e47819e8d418c79
Joshua Kissoon <JoshuaKissoon@gmail.com>
2015-01-05 12:45:31 +0800

    Made Route a database object

--- system/classes/Route.php
+++ system/classes/Route.php
@@ -6,13 +6,14 @@
      * @author Joshua Kissoon
      * @since 20150103
      */
-    class Route
+    class Route implements DatabaseObject
     {
 
-        private $url;
-        private $callback;
-        private $permission;
-        private $httpMethod;
+        private $rid;   // Unique ID for this route
+        private $url;   // Which URL does this route handle
+        private $callback;  // Callback function for this route
+        private $permission;    // Which permission is required to access this route
+        private $httpMethod;    // What method does this route handle
 
         public function __construct($url, $callback, $permission = "", $httpMethod = "GET")
         {
@@ -40,6 +41,54 @@
         public function getHTTPMethod()
         {
             return $this->httpMethod;
+        }
+
+        public function getId()
+        {
+            return $this->rid;
+        }
+
+        public static function isExistent($id)
+        {
+            
+        }
+
+        public function hasMandatoryData()
+        {
+            if ("" == $this->url || null == $this->url)
+            {
+                return false;
+            }
+            if ("" == $this->callback || null == $this->callback)
+            {
+                return false;
+            }
+            if ("" == $this->permission || null == $this->permission)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public function insert()
+        {
+            
+        }
+
+        public function load()
+        {
+            
+        }
+
+        public function update()
+        {
+            
+        }
+
+        public static function delete($id)
+        {
+            
         }
 
     }
