2e58994a4c94f1b933e27c02a5b778aaf686b093
Joshua Kissoon <JoshuaKissoon@gmail.com>
2015-01-07 17:28:40 +0800

    Added an API folder that handles API calls

---
+++ api/index.php
@@ -0,0 +1,36 @@
+<?php
+
+    /**
+     * Index file for the API
+     */
+    error_reporting(E_ALL | E_WARNING | E_NOTICE);
+    ini_set('display_errors', TRUE);
+
+    /**
+     * Lets bootstrap the system
+     */
+    require_once '../system/bootstrap.inc.php';
+
+    /**
+     * @section Load the modules for this url 
+     */
+    $handlers = JPath::getRoutes();
+    foreach ($handlers as $handler)
+    {
+        if (null == $handler->getPermissionId() || "" == $handler->getPermissionId())
+        {
+            /* There is no permission for this module at the current URL, just load it */
+            include_once JModuleManager::getModule($handler->getModule());
+        }
+        //else if ($codeli->getUser()->hasPermission($handler->getPermissionId()))
+        //{
+        /* If the user has the permission to access this module for this URL, load the module */
+        include_once JModuleManager::getModule($handler->getModule());
+        //}
+
+        $response = call_user_func($handler->getCallback());
+        print $response->getJSONOutput();
+    }
+
+    exit;
+    
